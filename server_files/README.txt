967c424bf602c1f1b2b10ecabb720f88026f37ae
Zach <cellozack@gmail.com>
2022-04-28 05:00:40 -0600

    Initial Commit For Server Files
    Created initial server files to allow an easier time setting up a server.

---
+++ server_files/README.txt
@@ -0,0 +1,4 @@
+Make sure you have the latest version of Java 17 installed!
+
+To start server simply agree to the EULA after launching the "startserver.bat"
+Then launch the server again to start it up.
---
+++ server_files/run.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Forge requires a configured set of both JVM and program arguments.
+REM Add custom JVM arguments to the user_jvm_args.txt
+REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
+REM  pass them to this script directly
+:START
+java @user_jvm_args.txt -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=32M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 @libraries/net/minecraftforge/forge/1.18.2-40.1.0/win_args.txt nogui %*
+
+echo Restarting automatically in 10 seconds (press Ctrl + C to cancel)
+timeout /t 10 /nobreak > NUL
+goto:START
+pause
